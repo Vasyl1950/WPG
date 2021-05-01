08291d47b3525cb93db3f20994d3c5e46ac6586d
Vasyl <yaremkv@mail.com>
2021-05-01 17:13:20 +0300

    js eror

--- src/js/index.js
+++ src/js/index.js
@@ -44,7 +44,7 @@ document.addEventListener('DOMContentLoaded', function () {
   if (window.location.pathname) {
     const activeCssClass = 'header__item--active';
    
-    const currentAnchor = document.querySelector(`a[href$='${path}']`);
-    currentAnchor.classList.add(activeCssClass);
+   
+   
   }
 });
