b98f90eed49fc379d3748486f696cde9f509210f
Vasyl <yaremkv@mail.com>
2021-05-01 23:18:42 +0300

    terser

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const CleanWebpackPlugin = require("clean-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 
 
 
